1b09c309065b5f43d5b02d6fc5b0ff19920c056e
supriya0311 <supriyapriyarl01@gmail.com>
2023-02-09 18:16:13 +0530

    heyyyy

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test 
+{
+	public static void main(String[]args)
+	{
+		String str="belakina    kavithe     beragige    sothe";
+		System.out.println(str);
+		System.out.println(str.replaceAll(" +",""));
+	}
+}
